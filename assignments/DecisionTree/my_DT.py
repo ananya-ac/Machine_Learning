5f9c54b0fc6f15b97ac6f81d8792f5f61e0cbe1f
Ananya <ananya@MacBook-Air.local>
2023-10-17 15:27:34 -0400

    refactoring of Decision Tree

--- assignments/DecisionTree/my_DT.py
+++ assignments/DecisionTree/my_DT.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
 from collections import Counter
-from Node import Node
 
 class my_DT:
 
-    def __init__(self, criterion="entropy", max_depth=8, min_impurity_decrease=0, min_samples_split=2):
+    def __init__(self, criterion="entropy", max_depth=4, min_impurity_decrease=1e-4, min_samples_split=2):
         #DID NOT USE HINT FILE
         # criterion = {"gini", "entropy"},
         # Stop training if depth = max_depth. Depth of a binary tree: the max number of edges from the root node to a leaf node
@@ -118,6 +117,100 @@ class my_DT:
         probs=X.apply(self._probsPerRow, axis='columns')
         probs.replace(np.NaN,0.0,inplace=True)
         return probs
+
+
+class Node(object):
+    
+    def __init__(self, X, y, depth, is_root=False, P=None, parent=None):
+        
+        self.split_features=None
+        self.is_root=is_root
+        self.X=X
+        self.y=y
+        self.P=P
+        self.parent=parent
+        self.leftChild=None
+        self.rightChild=None
+        self.depth=depth
+
+    def trySplit(self, features, criterion, split_thresholds=None,min_impurity_decrease=0, min_samples_split=2):
+        
+        if criterion=='gini':
+             metric=gini
+        if criterion=='entropy':
+             metric=entropy
+        if len(self.X)>=min_samples_split:
+            
+            
+            featureThreshList=[(feat,thresh) for feat in features for thresh in split_thresholds[feat][1:-1]]
+            feat,thresh=max(featureThreshList,key= lambda featureThresh:metricImprovement(self,featureThresh,metric))
+            # if self.depth==2:
+            #pdb.set_trace()
+            metric_improvement=metricImprovement(self,(feat,thresh),metric)
+            if metric_improvement<min_impurity_decrease:
+                 return False
+            else:
+                self.split_features=(feat,thresh)
+                leftX=self.X[self.X[feat]<thresh].copy()
+                rightX=self.X[self.X[feat]>=thresh].copy()
+                if not leftX.empty:
+                        self.leftChild=Node(X=leftX,y=self.y[self.X[feat]<thresh].copy(),parent=self,depth=self.depth+1)
+                if not rightX.empty:
+                        self.rightChild=Node(X=rightX,y=self.y[self.X[feat]>=thresh].copy(),parent=self,depth=self.depth+1)
+                return True
+                    
+        else:
+            return False
+           
+           
+    def getPred(self):
+
+        return max(Counter(self.y))
+    
+    def getProb(self):
+        counts=pd.Series(Counter(self.y))
+        return counts/sum(counts)
+        
+    def __str__(self):
+
+        return str(self.split_features) + "\n" + str(Counter(self.y)) 
+
+def entropy(y):
+    y=pd.Series(Counter(y))
+    y/=y.sum()
+    ylog=np.emath.logn(3,y)
+    y*=ylog
+    
+    return -1*y.sum()
+
+
+def metricImprovement(node, featureThresh, metric):
+    
+    parent_metric=metric(node.y)
+    feat,thresh=featureThresh
+    left=node.y[node.X[feat]<thresh]
+    right=node.y[node.X[feat]>=thresh]
+    if left.empty:
+        left_metric=0
+    else: 
+        left_metric=metric(left)
+    if right.empty:
+        right_metric=0
+    else: 
+        right_metric=metric(right)
+    left_metric*=(len(left)/len(node.y))
+    right_metric*=(len(right)/len(node.y))
+    return parent_metric-(left_metric+right_metric)
+
+
+def gini(y):
+    y=pd.Series(Counter(y))
+    y/=y.sum()
+    return 1 - (y**2).sum()
+
+
+
+    
 
 
 
