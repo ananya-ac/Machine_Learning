ea2dead94362081f948dd75d8c73282f4018af64
Ananya <ananya@MacBook-Air-3.lan>
2023-10-26 04:34:27 -0400

    assignment 8

--- assignments/Kmeans/my_KMeans.py
+++ assignments/Kmeans/my_KMeans.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import pdb
 
 class my_KMeans:
 
@@ -17,21 +18,84 @@ class my_KMeans:
 
         self.classes_ = range(n_clusters)
         # Centroids
-        self.cluster_centers_ = None
         # Sum of squared distances of samples to their closest cluster center.
         self.inertia_ = None
+        self.cluster_centers_={}
 
     def fit(self, X):
         # X: pd.DataFrame, independent variables, float        
         # repeat self.n_init times and keep the best run 
             # (self.cluster_centers_, self.inertia_) with the lowest self.inertia_.
         # write your code below
+        cluster_centers={}
+        for i in range(self.n_init):
+            
+            if self.init=='random':
+                initial_centroids=X.iloc[np.random.choice(len(X),size=self.n_clusters,replace=False)].values
+                
+            if self.init=='k-means++':
+                rand_index=np.random.choice(len(X))
+                first_centroid=X.iloc[rand_index].values
+                prob=self.euclidean_distance(X,first_centroid)/sum(self.euclidean_distance(X,first_centroid))
+                other_centroids=X.iloc[np.random.choice(len(X),p=prob,size=self.n_clusters-1,replace=False)].values
+                initial_centroids=np.vstack([first_centroid,other_centroids])
+
+            centroids,inertia=self.get_clusters_(initial_centroids,X)
+            
+
+            cluster_centers[inertia]=centroids
+                
+        best_inertia=min(cluster_centers.keys())
+        self.inertia_=best_inertia
+        self.cluster_centers_=[row for row in cluster_centers[best_inertia]]
         return
+
+    def euclidean_distance(self,a,b):
+        return np.sum((np.array(a)-np.array(b))**2,axis=1)**0.5
+
+    def get_clusters_(self,centroids,X):
+
+        for i in range(self.max_iter):
+                dists=[]
+                
+                for j in range(len(centroids)):
+                    
+                    SE=self.euclidean_distance(X,centroids[j])
+                    dists.append(SE)
+                    
+                
+                dists_arr=np.array(dists).T
+                X['cluster']=np.argmin(dists_arr,axis=1)
+                clusters_group=X.groupby('cluster')
+                inertia=0
+                
+                for k in range(self.n_clusters):
+                        inertia+=sum(self.euclidean_distance(clusters_group.get_group(k).drop('cluster',axis='columns'),centroids[k]))
+                                
+                if i>0:
+                    if prev_inertia-inertia<self.tol:
+                        X.drop('cluster', inplace=True, axis='columns')
+                        break
+                
+                prev_inertia=inertia   
+                centroids=clusters_group.mean().values
+                X.drop('cluster', inplace=True, axis='columns')
+                
+                
+        
+                
+        return (centroids,inertia)
+
+    
 
     def predict(self, X):
         # X: pd.DataFrame, independent variables, float
         # return predictions: list
         # write your code below
+        dists=self.transform(X)
+        predictions = [np.argmin(dist) for dist in dists]
+
+
         return predictions
 
     def transform(self, X):
@@ -39,6 +103,8 @@ class my_KMeans:
         # X: pd.DataFrame, independent variables, float
         # return dists = list of [dist to centroid 1, dist to centroid 2, ...]
         # write your code below
+        #pdb.set_trace()
+        dists = [[np.linalg.norm(x-centroid) for centroid in self.cluster_centers_] for x in X.to_numpy()]
         return dists
 
     def fit_predict(self, X):
