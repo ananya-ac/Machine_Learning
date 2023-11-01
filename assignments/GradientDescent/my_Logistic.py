4ec463159152170577c028da218bf1c03cff32e8
Ananya <ananya@MacBook-Air-3.lan>
2023-11-01 04:06:49 -0400

    Assignment 9 initial push

--- assignments/GradientDescent/my_Logistic.py
+++ assignments/GradientDescent/my_Logistic.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+import pdb
 class my_Logistic:
 
     def __init__(self, learning_rate = 0.1, batch_size = 10, max_iter = 100, shuffle = False):
@@ -14,33 +14,89 @@ class my_Logistic:
         self.batch_size = batch_size
         self.max_iter = max_iter
         self.shuffle = shuffle
+        
+    
+        
+    
+
 
     def fit(self, X, y):
         # X: pd.DataFrame, independent variables
         # y: list, np.array or pd.Series, dependent variables
         data = X.to_numpy()
+        data=np.concatenate([data,np.ones(shape=(data.shape[0],1))],axis=1)
         d = data.shape[1]
         # Initialize weights as all zeros
-        self.w = np.array([0.0]*d)
-        self.w0 = 0.0
+        self.w = np.array([0.0]*(d))
         # write your code below
+        for i in range(self.max_iter):
+            
+            if self.shuffle:
+                inds=np.random.permutation(range(len(data)))                    
+            else:
+                inds=np.array(range(len(data)))
+            
+            num_inds=len(inds)
+            
+            while num_inds>0:
+                
+                num_inds-=self.batch_size
+                
+                if num_inds>self.batch_size:
+                    b_inds=inds[:self.batch_size]
+                    inds=inds[self.batch_size:]
+                
+                else:
+                    b_inds=inds
+
+                Xb , yb =data[b_inds], y[b_inds]
+                self.batch_update(Xb,yb)
+                
+
+               
+                
+            
+            # for j in np.ceil(data.shape[0]/self.batch_size):
+            #     if data.shape[0]<self.batch_size:
+            #         X_batch=data[-len(indices):]
+            #         y_batch=y[-len(indices):]
+            #     b_indices=indices[:self.batch_size]
+            #     X_batch=data[b_indices]
+            #     y_batch=y[b_indices]
+            #     indices=indices[self.batch_size:]
+
+    def batch_update(self,Xb,yb):
+        
+        gradient=grad(wts=self.w,Xb=Xb,yb=yb)
+        self.w-=self.learning_rate*gradient
+
+
+           
 
     def predict_proba(self, X):
         # X: pd.DataFrame, independent variables
         # prob is a dict of prediction probabilities belonging to each categories
         # return probs = f(x) = 1 / (1+exp(-(w0+w*x))}); a list of float values in [0.0, 1.0]
         # write your code below
-        return fx
+        X=np.concatenate([X,np.ones(shape=(X.shape[0],1))],axis=1)
+        probs=sigmoid(X@self.w)
+        return probs
 
     def predict(self, X):
         # X: pd.DataFrame, independent variables, str
         # return predictions: list of int values in {0, 1}
-        # write your code below
+        
         probs = self.predict_proba(X)
         predictions = [1 if prob >=0.5 else 0 for prob in probs]
         return predictions
 
 
+def grad(wts, Xb, yb):
 
+    m=len(yb)
+    return (1/m) * Xb.T @ (sigmoid(Xb@wts)-yb)
+
+def sigmoid(x):
+        return 1/(1+np.exp(-x))
 
 
